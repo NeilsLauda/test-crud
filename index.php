5724dba1084b6f13f9bcdcaf75b9e56e26f35cbc
Neils Mata <neilsmata@hotmail.com>
2022-03-02 21:48:16 -0500

    add logic

--- index.php
+++ index.php
@@ -20,10 +20,10 @@
       <div class="card card-body">
         <form action="save_task.php" method="POST">
           <div class="form-group">
-            <input type="text" name="title" class="form-control" placeholder="Titulo de la tarea" autofocus>
+            <input type="text" name="title" class="form-control" placeholder="nombre de skill" autofocus>
           </div>
           <div class="form-group">
-            <textarea name="description" rows="2" class="form-control" placeholder="Descripcion de la tarea"></textarea>
+            <textarea name="description" rows="2" class="form-control" placeholder="Descripcion del skill"></textarea>
           </div>
           <input type="submit" name="save_task" class="btn btn-success btn-block" value="Guardar tarea">
         </form>
